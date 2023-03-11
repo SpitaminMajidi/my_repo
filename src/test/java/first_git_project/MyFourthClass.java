e92bebe1ed17d7abfe9080ada99d6299b4ffd105
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2023-03-11 20:47:09 +0100

    new

--- src/test/java/first_git_project/MyFourthClass.java
+++ src/test/java/first_git_project/MyFourthClass.java
@@ -3,5 +3,7 @@ package first_git_project;
 public class MyFourthClass {
     public static void main(String[] args) {
         System.out.println("asdf");
+        System.out.println();
+        System.out.println();
     }
 }
