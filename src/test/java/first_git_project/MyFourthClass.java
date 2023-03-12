1e8ac68e52c96dab0a953305c359537eee55d601
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2023-03-12 15:02:03 +0100

    commit only third class

--- src/test/java/first_git_project/MyFourthClass.java
+++
@@ -1,9 +0,0 @@
-package first_git_project;
-
-public class MyFourthClass {
-    public static void main(String[] args) {
-        System.out.println("asdf");
-        System.out.println();
-        System.out.println();
-    }
-}
