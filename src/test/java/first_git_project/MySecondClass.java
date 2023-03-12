922ca8426acbccdadc5d698c9a716a772ccc9fd0
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2023-03-12 14:51:30 +0100

    I have updated my code

--- src/test/java/first_git_project/MySecondClass.java
+++ src/test/java/first_git_project/MySecondClass.java
@@ -2,6 +2,9 @@ package first_git_project;
 
 public class MySecondClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World *");
+
+
+        System.out.println("Atila's code");
     }
 }
