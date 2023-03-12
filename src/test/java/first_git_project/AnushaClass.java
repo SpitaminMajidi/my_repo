6ec3edf61714481d2f48a225dfeb5ee97d5e7a11
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2023-03-12 18:06:17 +0100

    Annuu

--- src/test/java/first_git_project/AnushaClass.java
+++ src/test/java/first_git_project/AnushaClass.java
@@ -1,7 +1,8 @@
 package first_git_project;
 
-public class MySeventhClass {
+public class AnushaClass {
     public static void main(String[] args) {
-        System.out.println("anusha jan");
+
+        System.out.println("Anusha Jan");
     }
 }
